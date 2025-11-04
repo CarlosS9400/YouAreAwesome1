2832ba9a26662c6a1ee41bb46a2fd84c1a38472e
Carlos <gotgoodjob@gmail.com>
2025-11-04 12:10:06 +0800

    Initial Commit

---
+++ YouAreAwesome1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YouAreAwesome1
+//
+//  Created by Carlos Antonio dos Santos on 04/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YouAreAwesome1/YouAreAwesome1App.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesome1App.swift
+//  YouAreAwesome1
+//
+//  Created by Carlos Antonio dos Santos on 04/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesome1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
